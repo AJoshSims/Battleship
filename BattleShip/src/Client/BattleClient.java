7895138c2c1536fa7cf426f4e91f595afc6dc2c3
Evan Arroyo <Evan Arroyo>
2016-11-06 16:49:57 -0500

    Added Required Files

---
+++ BattleShip/src/Client/BattleClient.java
@@ -0,0 +1,7 @@
+package Client;
+
+/**
+ * Created by Evan on 11/6/2016.
+ */
+public class BattleClient {
+}
---
+++ BattleShip/src/Common/ConnectionInterface.java
@@ -0,0 +1,13 @@
+package Common;
+
+/**
+ * Created by Evan on 11/6/2016.
+ */
+public class ConnectionInterface extends MessageSource implements Runnable{
+
+
+    @Override
+    public void run() {
+
+    }
+}
