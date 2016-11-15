2927dcf5ab28a0a6e18e5b89eba5ce6d14f45025
Evan Arroyo <Evan Arroyo>
2016-11-15 12:37:18 -0500

    Created board with user agrs size specification

--- BattleShip/src/Server/BattleShipDriver.java
+++ BattleShip/src/Server/BattleShipDriver.java
@@ -4,4 +4,35 @@ package Server;
  * Created by Evan on 11/6/2016.
  */
 public class BattleShipDriver {
+
+    public static void main(String args[]){
+
+        int portNum = 9999;
+        int height = 10;
+        int width = 10;
+
+        String[][] defBoardSize;
+
+        if(args.length == 0){
+            UsageMessage();
+        }
+
+        if(args.length == 1){
+            portNum = Integer.parseInt(args[0]);
+            defBoardSize = new String[height][width];
+        }
+
+        if(args.length == 2){
+            portNum = Integer.parseInt(args[0]);
+            height = Integer.parseInt(args[1]);
+            width = height;
+        }
+
+        Grid grid = new Grid(height,width);
+        grid.createBoard();
+
+    }
+    private static void UsageMessage(){
+        System.out.println("Usage: <port> <boardSize>");
+    }
 }
--- BattleShip/src/Server/Grid.java
+++ BattleShip/src/Server/Grid.java
@@ -3,5 +3,28 @@ package Server;
 /**
  * Created by Evan on 11/6/2016.
  */
-public class Grid {
+    public class Grid {
+
+    String[][] board;
+
+    public Grid(int height, int width){
+
+        board = new String[height][width];
+
+    }
+
+    public void createBoard(){
+
+        for (int i = 0; i < board.length;i++){
+
+            for (int j = 0; j < board.length;j++){
+
+                board[i][j] = "|__";
+
+                System.out.print(board[i][j] + "|");
+            }
+
+            System.out.println();
+        }
+    }
 }
